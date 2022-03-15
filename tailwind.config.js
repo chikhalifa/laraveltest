b52ba370752572cf85d77f969ce3f6a86c0d094e
chikhalifa <49573718+chikhalifa@users.noreply.github.com>
2022-03-15 14:20:04 +0100

    tuesday push

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./resources/views/layouts/app.blade.php",
+      "./resources/js/app.js",
+      "./resources/**/*.vue",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
